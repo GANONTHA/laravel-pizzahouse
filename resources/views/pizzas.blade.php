857cb8b7fcd3a12768bd5835afb23e9c4b1ab351
Bruno <Bruno>
2024-03-13 20:50:16 +0100

    changes

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -1,30 +1,27 @@
 @extends('layouts.layout')
 
 @section('content')
-<h1>Welcome to our Pizza House</h1>
-<p>Name: {{$type}}</p>
-<p>Age: {{$age}}</p>
-<table>
-    <tr>
-        <th>Name</th>
-        <th>Price</th>
-    </tr>
-    <tr>
+    <h1>Welcome to our Pizza House</h1>
+    <p>Name: {{$type}}</p>
+    <p>Age: {{$age}}</p>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Price</th>
+        </tr>
+        <tr>
+            @foreach ($pizzas as $pizza)
+                <tr>
+                    <td>  {{$pizza['name']}} </td>
+                    <td>  ${{$pizza['price']}}</td>
+                </tr>
+            @endforeach
+    </table>
 
-
-@foreach ($pizzas as $pizza)
-<tr>
-    <td>  {{$pizza['name']}} </td>
-    <td>  ${{$pizza['price']}}</td>
-</tr>
-@endforeach
-</table>
-
-{{-- Route parameter --}}
-
-@foreach ($pizzas as $pizza)
-    @if ($id == $pizza['id'])
-    <h1>This is pizza: {{$pizza['name']}} --- {{$pizza['id']}}</h1>
-    @endif
-@endforeach
+    {{-- Route parameter --}}
+    @foreach ($pizzas as $pizza)
+        @if ($id == $pizza['id'])
+            <h1>This is pizza: {{$pizza['name']}} --- {{$pizza['id']}}</h1>
+        @endif
+    @endforeach
 @endsection
