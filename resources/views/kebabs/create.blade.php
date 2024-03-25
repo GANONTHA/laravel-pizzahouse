b902e014c635c989779c26bcd96996b03a8b5dde
Bruno <bganontha@gmail.com>
2024-03-25 12:33:20 +0100

    Update input fields with placeholders

--- resources/views/kebabs/create.blade.php
+++ resources/views/kebabs/create.blade.php
@@ -5,9 +5,9 @@
 <form action="/kebabs" method="POST">
     @csrf
     <label for="name">Name:</label>
-    <input type="text" id="name" name="name">
+    <input type="text" id="name" name="name" placeholder="name">
     <label for="price">Price:</label>
-    <input type="text" id="price" name="price">
+    <input type="text" id="price" name="price" placeholder="price">
     <button type="submit" >Order</button>
 </form>
 @endsection
